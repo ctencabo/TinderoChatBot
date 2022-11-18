6f3e441d3abb22afdc3c091bcba69f0037f13929
Christian <encabo.chris@gmail.com>
2022-11-18 12:21:30 +0800

    add location handler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from constants import Api as api
 from telegram import \
     KeyboardButton, \
     ReplyKeyboardMarkup,\
@@ -6,8 +7,8 @@ from telegram import \
     InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
 
-with open('token.txt', 'r') as f:
-    TOKEN = f.read()
+TOKEN = api.TELEGRAM_BOT_API
+ZOMATO = api.ZOMATO_API
 
 yes_choice = "Yes"
 no_choice = "No"
@@ -27,6 +28,7 @@ def help(update, context):
     /start -> Start Tindero
     /help -> This message
     /suggest -> Tindero will ask a series of questions to help in suggesting a restaurant
+    /location -> Tindero will provide your current location for more accurate suggesting of restaurants
     """)
 
 def message_handler(update, context):
@@ -41,12 +43,22 @@ def suggest(update, context):
         "HELLOOOOO I CAN'T SUGGEST ANYTHING AT THE MOMENT",
         reply_markup=InlineKeyboardMarkup(buttons))
 
+def location_handler(update, context):
+    buttons = [[KeyboardButton(yes_choice, request_location=True)], [KeyboardButton(no_choice)]]
+    update.message.reply_text("Do you want to share your location?", reply_markup=ReplyKeyboardMarkup(buttons))
+
+def location(update, context):
+    user_location = update.message.location
+    update.message.reply_text(f'You are currently in LATITUDE: {user_location.latitude} and LONGITUDE: {user_location.longitude}', reply_markup=ReplyKeyboardRemove(True))
+
 updater = Updater(TOKEN, use_context=True)
 disp = updater.dispatcher
 
 disp.add_handler(CommandHandler("start", start))
 disp.add_handler(CommandHandler("help", help))
 disp.add_handler(CommandHandler("suggest", suggest))
+disp.add_handler(CommandHandler("location", location_handler))
+disp.add_handler(MessageHandler(Filters.location, location))
 disp.add_handler(MessageHandler(Filters.text, message_handler))
 
 updater.start_polling()
